feaf60abe18a891d292041a1749a0cb6a52a0733
Grosus <114068852+Grosus@users.noreply.github.com>
2022-10-21 12:57:57 +0200

    test

--- PONCET_Ronan_1_dashboard_102022.py
+++ PONCET_Ronan_1_dashboard_102022.py
@@ -8,7 +8,7 @@ import streamlit as st
 import seaborn as sns
 import os
 from sklearn import metrics
-from sklearn.metrics import f1_score, roc_auc_score, PrecisionRecallDisplay, RocCurveDisplay, log_loss
+from sklearn.metrics import recall_score, roc_auc_score, PrecisionRecallDisplay, RocCurveDisplay, fbeta_score,precision_score
 import joblib
 import shap
 import pickle
@@ -19,23 +19,26 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 
-
+# Lit le fichier nécéssaire au dashboard
 @st.cache
 def read(): 
     path= "./" 
     data=pd.read_csv(os.path.join(path,'df_prepro.csv'))
-    data = data[data['TARGET'].notnull()].dropna(1).drop(['index','Unnamed: 0.1','Unnamed: 0'],1)
+    df = data[data['TARGET'].notnull()].drop('Unnamed: 0',1)
     results=pd.read_csv(os.path.join(path,'prediction.csv'))
     loaded_model = joblib.load('model_lgbm.pkl')
     results['TARGET']=results.TARGET.round(2)
-    
-    shap_values = np.load('shap_value.npy')    
+    with open("shap_value", "rb") as fp:   # Unpickling
+        shap_values = pickle.load(fp)   
     expected_values =np.load('explainer.npy')
-    return data , results , loaded_model,shap_values,expected_values  
+    return df , results , loaded_model,shap_values,expected_values  
 
+# Plot de la partie client pour un client et une variable
 def plot(df,options,number ):
     value_client=df.query('SK_ID_CURR == @number')[options]
+    ymax=df.groupby(options).count().max()[0]
     if len(df[options].unique())<20:
+        st.write('Valeurs de la variable pour le client :' ,value_client )
         fig, ax = plt.subplots()
         df_0=df[df['TARGET']==0]
         df_1=df[df['TARGET']==1]
@@ -46,16 +49,15 @@ def plot(df,options,number ):
         cat_plot.plot(kind='bar',ax=ax)
         value_client=df.query('SK_ID_CURR == @number')[options]
         ax.plot(value_client,1 , marker = '|', linestyle = '' , color='green')
-    else:     
+        plt.vlines(value_client,0,ymax, color='r', label='client')
+    else:
+        st.write('Valeurs de la variable pour le client :' ,value_client)
         fig=sns.displot(x=options,data=df,hue='TARGET')
-        ymax=df.groupby(options).count().max()[0]
-        
-        st.write(df.groupby(options).count())
         plt.vlines(value_client,0,ymax, color='r', label='client')
     
     return fig
     
-    
+#Selection de variable pour la fonction plot   
 def client_plot(df,number):
     columns=list(df.drop(['SK_ID_CURR','TARGET'],1).columns)
     columns.insert(0,'Aucun')
@@ -66,11 +68,15 @@ def client_plot(df,number):
     if option=='Aucun' :
         st.write('Choisissez une variable')
         st.stop()
-    st.write('You selected:', option)
     fig = plot(df , option,number)
     st.pyplot(fig)
-
+    
+#Fonction général de la partie client
 def explo_plot(df,clf,shap_values,expected_values):
+    st.title('Information sur les clients')
+    st.write("Veuillez inserer un numero de client valide pour continuer l'éxploration")
+    st.write("Comme ce Dashboard ne visualise qu'un nombre restreint de donnée ,des exemple d'ID clients valide peuvent etre trouvé dans la colonne SK_ID_CURR du fichier df_prepro.csv")
+    
     tresh=50
     number = st.number_input('Inserez le numero de client',min_value=0, max_value=999999)
     if number==0:
@@ -87,14 +93,16 @@ def explo_plot(df,clf,shap_values,expected_values):
     col1, col2 = st.columns(2)
     
     col1.metric("TARGET", df.query("SK_ID_CURR == @number")['TARGET'])
-    col2.metric("Prediction", round(y_pred[index,1],2))
+    col2.metric("Prediction d'appertenance a la classe 1", round(y_pred[index,1],2))
+    
+    
     if st.radio(
     "",
     ('Informations général', 'Importance des features dans la prediction'),horizontal=True )=='Informations général':
         client_plot(df,number)
     else:
         (X,y,y_pred)=result_pred(df,clf)
-        shap_values_explaination = shap.Explanation(shap_values[index,:], feature_names=X.columns.tolist()) 
+        shap_values_explaination = shap.Explanation(shap_values[1][index,:], feature_names=X.columns.tolist()) 
         fig, ax = plt.subplots()
         ax=shap.plots._waterfall.waterfall_legacy(expected_values[1],shap_values_explaination.values,
                                                   feature_names=X.columns.tolist(),max_display=20)
@@ -105,9 +113,9 @@ def explo_plot(df,clf,shap_values,expected_values):
 def button(df):
     page = st.sidebar.radio(
     "selectionnez",
-    ('acceuil','client', 'model','obtenir une prediction'),index=0)
+    ('Acceuil','Client', 'Model','Prédiction'),index=0)
     return page
-
+#Renvoie la Target en fonction du client
 def result_pred(df,clf):
     
     X=df.drop(['TARGET','SK_ID_CURR'],1).copy()
@@ -117,20 +125,22 @@ def result_pred(df,clf):
 
     return plt
 
+#Ecrit les métriques dans la parti modèle
 def metrique(X,y,clf,y_pred,y_tresh):
 
 
     
+    score_recall=recall_score(y_tresh,y).round(2)
+    score_précision=precision_score(y_tresh,y).round(2)
+    auc_score=roc_auc_score(y_tresh,y).round(2)
+    score_f1=fbeta_score(y_tresh,y,beta=2).round(2)
     
-    log_loss_score=log_loss(y,y_pred)
-    auc_score=roc_auc_score(y_tresh,y)
-    score_f1=f1_score(y_tresh,y)
-    
-    col1, col2, col3 = st.columns(3)
-    col1.metric("log loss", log_loss_score)
-    col2.metric("auc score",auc_score)
-    col3.metric("f1 score", score_f1)
-    
+    col1, col2, col3,col4 = st.columns(4)
+    col1.metric("Recall", score_recall)
+    col2.metric("Precision", score_précision)
+    col3.metric("AUC score",auc_score)
+    col4.metric("F_beta score", score_f1)
+#Génere les shap plot de la partie modèle    
 def shap_plot_mdl(shap_values,X,df):
     shap.initjs()
     columns=list(df.drop(['SK_ID_CURR','TARGET'],1).columns)
@@ -138,17 +148,17 @@ def shap_plot_mdl(shap_values,X,df):
     option = st.selectbox(
             'choix de la variable',
             columns)
+    shap_value=[shap_values[0],shap_values[1]]
     if option=='Informations générales':
-        st.write(shap_values.shape)
         fig, ax = plt.subplots()
-        ax=shap.summary_plot(shap_values, X,max_display=10)
+        ax=shap.summary_plot(shap_value, X,max_display=10)
         st.pyplot(fig)
     else:
         fig, ax = plt.subplots()
-        fig = shap.dependence_plot(option, shap_values, X, display_features=X)
+        fig = shap.dependence_plot(option, shap_value[1], X, display_features=X)
         st.pyplot(fig)
     
-        
+#Matrice de confusion pour la partie model      
 def pred_plot_mdl(y_tresh, y,df):
     #    shap_plot
 
@@ -160,6 +170,7 @@ def pred_plot_mdl(y_tresh, y,df):
     df_cm = pd.DataFrame(conf_mat, index = [label for label in set(y)],
                       columns = [i for i in "01"])
     ax=sns.heatmap(df_cm, annot=True, cmap="Blues")
+    ax.set(xlabel='Y_pred', ylabel='Ytrue',title='Matrice de confusion')
     col1.pyplot(fig)
     number = col2.number_input("Entrez l'id client",min_value=0, max_value=999999)
     if number==0:
@@ -173,16 +184,18 @@ def pred_plot_mdl(y_tresh, y,df):
     
 
     
-
+#Fonction général de la partie modèle
 def model(df,clf,shap_values,expected_values):
     
     st.title('Information général sur le modèle')
     
     (X,y,y_pred)=result_pred(df,clf)
     col1, col2 = st.columns(2)
-    
+    col1.write('Roc curve')
     fig_ROC = RocCurveDisplay.from_estimator(clf, X, y)
+    
     col1.pyplot(fig_ROC.figure_)
+    col2.write('Precision Recall curve')
     fig_ROC = PrecisionRecallDisplay.from_estimator(clf, X, y)
     col2.pyplot(fig_ROC.figure_)
     tresh_min=round((y_pred[:,1].min()*100)+1)
@@ -210,8 +223,9 @@ def model(df,clf,shap_values,expected_values):
         shap_plot_mdl(shap_values,X,df)
     
     
-    
+#Fonction pour la prédiction d'un nouveau clients    
 def predict_new(df):
+    st.title("Prédiction d'un nouveau client")
     data_name=['application_train.csv','application_test.csv', 'bureau.csv' ,'bureau_balance.csv' , 'credit_card_balance.csv', 'installments_payments.csv', 'POS_CASH_balance.csv', 'previous_application.csv']
     st.write("Pour obtenir la prédiction d'un nouveau client vous devez ajouter 7 Dataframes nommés : ")
     
@@ -222,7 +236,6 @@ def predict_new(df):
         st.write("filename:", uploaded_file.name)
         data=pd.read_csv(BytesIO(bytes_data))
         dicti[uploaded_file.name[:-4]]=data.to_json()
-    st.write(dicti.keys())
     
     if len(dicti) == 8:
             
@@ -238,35 +251,33 @@ def predict_new(df):
         try:
             
             response = requests.post('https://p7apirp.herokuapp.com/predict', json=dicti)
-            st.write(response.content)
-            
             
             response = requests.post('https://p7apirp.herokuapp.com/prepro', json=dicti)
             response=json.loads(response.content.decode("utf-8").replace("'",'"'))
             client_data=pd.DataFrame.from_dict(response,orient='index')
             
+            number=client_data['SK_ID_CURR'][0]
+            
+
+            
+            client_data['TARGET']=[1]
+            st.write(client_data)
+            df=df[client_data.columns]
+            df=pd.concat([df, client_data])
+
+            client_plot(df,number)
+            
         except:
-            st.write('error')
+            pass
             
     
 
-    number=client_data['SK_ID_CURR'][0]
-    df=df[client_data.columns]
-    df=df.merge(client_data, left_on='SK_ID_CURR', right_on='SK_ID_CURR')
-    st.write(df)
-    
-    client_plot(df,number)
+   
     
     
 
             
-    
-        
-
-    
-    
- 
-            
+#Page d'acceuil
 def acceuil():
     st.title('Ronan PONCET')
     st.title('Projet 7 implementez un model de scoring')
@@ -286,18 +297,18 @@ def acceuil():
 #    st.write('goodbye')
     
     
-        
+#Fonction principal        
 def main():
     
     (df,result,clf,shap_values,expected_values)=read()
     page=button(df)
         
         
-    if (page=='acceuil'):
+    if (page=='Acceuil'):
         acceuil()
-    elif (page=='client'):
+    elif (page=='Client'):
         explo_plot(df,clf,shap_values,expected_values)
-    elif(page=='model') :
+    elif(page=='Model') :
         model(df,clf,shap_values,expected_values)
     else :
         predict_new(df)
